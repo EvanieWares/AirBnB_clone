7e393877a4015b2681ec1f6989187d770008750b
Kali <effahfrancis210@gmail.com>
2023-11-07 10:42:50 -0500

    update

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+Script for our base class
+"""
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """Create a base class and assign public
+    instance attribute
+    """
+
+    def __init__(self):
+        """initialize the class"""
+        self.id = str(uuid.uuid4())
+        self.created_at =  datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f'{self.__class__.__name__} ({self.id}) {self.__dict__}'
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.created_at.isoformat()
+        return obj_dict
