0a472295eb71848e5320af768b962ed9a106acdf
effah001 <effahfrancis210@gmail.com>
2023-11-07 15:20:45 -0500

    update

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,22 @@ class BaseModel:
     instance attribute
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """initialize the class"""
-        self.id = str(uuid.uuid4())
-        self.created_at =  datetime.now()
-        self.updated_at = datetime.now()
+        for key, value in kwargs.items():
+            if key != "__class__":
+                if key == "created_at":
+                    self.__dict__['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "updated_at":
+                    self.__dict__['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "id":
+                    setattr(self, key, str(value))
+                else:
+                    setattr(self, key, value)
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at =  datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -33,5 +44,5 @@ class BaseModel:
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
