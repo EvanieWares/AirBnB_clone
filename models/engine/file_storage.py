4de85db820c19c54b80d7a817b5a53e5e3dec2c7
evaniewares <chisopsyelera@gmail.com>
2023-11-12 13:50:59 +0000

    fixed attribute issues

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,31 +16,30 @@ from models.review import Review
 class FileStorage:
     """A class that serializes instances to JSON"""
 
-    def __init__(self, file_path="file.json"):
-        self.__file_path = file_path
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
         """Returns dictionary objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         serialized_objects = {}
-        for key, obj in self.__objects.items():
+        for key, obj in FileStorage.__objects.items():
             serialized_objects[key] = obj.to_dict()
-        with open(self.__file_path, "w", encoding="utf-8") as jf:
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as jf:
             json.dump(serialized_objects, jf)
 
     def reload(self):
         """Deserializes the JSON file to objects"""
         try:
-            with open(self.__file_path, "r", encoding="utf-8") as jf:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as jf:
                 serialized_objects = json.load(jf)
             for key, obj_dict in serialized_objects.items():
                 self.new(eval(key.split(".")[0])(**obj_dict))
